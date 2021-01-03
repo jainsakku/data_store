6dc741fef8bf6919092b5015832910a57cc0fc98
Saksham Jain <jainsaksham36b@gmail.com>
2021-01-03 14:28:36 +0530

    sanity check added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from arctic.date import DateRange
 from flask import Flask
 import ccxt
 import time
@@ -8,11 +9,17 @@ from arctic import Arctic
 import sys
 import concurrent.futures
 import pandas as pd
+import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 app = Flask(__name__)
 crontab = Crontab(app)
 store = Arctic("localhost")
-store.initialize_library("BINANCE_TEST")
+store.initialize_library("BINANCE_TEST_10")
 
 
 @crontab.job()
@@ -23,12 +30,19 @@ def process_1m_data():
     def _fetch_result(symbol):
         data = binance.fetch_ohlcv(symbol, "1m", int((time.time() // 60 - 1) * 60000))
         r = redis.Redis(host='localhost', port=6379, db=0)
-        library = store['BINANCE_TEST']
+        library = store['BINANCE_TEST_10']
         if len(data) > 0:
             if not r.exists(str(int((time.time() // 60) * 60000)) + symbol + "-1m"):
-                df = pd.DataFrame([data[0]], columns=['t', 'o', 'h', 'l', 'c', 'v'])
-                r.set(str(int((time.time() // 60) * 60000)) + symbol + "-1m", str(json.dumps(data[0])))
-                library.append(symbol + "-1m", df)
+                try:
+                    # print(data[0][0])
+                    data[0][0] = pd.to_datetime(data[0][0] / 1000, unit='s').tz_localize("UTC")
+                    df = pd.DataFrame([data[0]], columns=['t', 'o', 'h', 'l', 'c', 'v'])
+                    df.set_index('t')
+                    r.set(str(int((time.time() // 60) * 60000)) + symbol + "-1m", str(json.dumps(data[0])))
+                    library.write(symbol + "-1m", df)
+                    print(symbol)
+                except Exception as e:
+                    print(e)
             return data[0]
 
         return []
@@ -86,7 +100,9 @@ def process_5m_data():
         output_list = [int((time.time() // 60) * 60000), open, high, low, close, volume]
         if not r.exists(str(int((time.time() // 60) * 60000)) + symbol + "-5m"):
             r.set(str(int((time.time() // 60) * 60000)) + symbol + "-5m", str(json.dumps(output_list)))
+            output_list[0] = pd.to_datetime(output_list[0] / 1000, unit='s').tz_localize("UTC")
             df = pd.DataFrame([output_list], columns=['t', 'o', 'h', 'l', 'c', 'v'])
+            df.set_index('t')
             library.write(symbol + '-5m', df)
 
     end = time.time()
@@ -121,10 +137,12 @@ def process_15m_data():
                 high = max(high, row[2])
                 low = min(low, row[3])
                 r.delete(key)
-        output_list = [int(time.time() * 1000), open, high, low, close, volume]
+        output_list = [int(time.time()), open, high, low, close, volume]
         if not r.exists(str(int((time.time() // 60) * 60000)) + symbol + "-15m"):
             r.set(str(int((time.time() // 60) * 60000)) + symbol + "-15m", str(json.dumps(output_list)))
+            output_list[0] = pd.to_datetime(output_list[0], unit='s').tz_localize("UTC")
             df = pd.DataFrame([output_list], columns=['t', 'o', 'h', 'l', 'c', 'v'])
+            df.set_index('t')
             library.write(symbol + '-15m', df)
 
     return "15m job executed successfully"
@@ -160,8 +178,10 @@ def process_30m_data():
         output_list = [(int(time.time() // 60) * 60000), open, high, low, close, volume]
         if not r.exists(str(int((time.time() // 60) * 60000)) + symbol + "-30m"):
             r.set(str(int((time.time() // 60) * 60000)) + symbol + "-30m", str(json.dumps(output_list)))
-            pd.DataFrame([output_list], columns=['t', 'o', 'h', 'l', 'c', 'v'])
-            library.write(symbol + '-30m', pd)
+            output_list[0] = pd.to_datetime(output_list[0] / 1000, unit='s').tz_localize("UTC")
+            df = pd.DataFrame([output_list], columns=['t', 'o', 'h', 'l', 'c', 'v'])
+            df.set_index('t')
+            library.write(symbol + '-30m', df)
 
     return "30m job executed successfully"
 
@@ -170,8 +190,8 @@ def process_30m_data():
 @app.route('/get60m')
 def process_60m_data():
     time.sleep(40)
-    symbols = json.loads(r.get("symbols"))
     r = redis.Redis(host='localhost', port=6379, db=0)
+    symbols = json.loads(r.get("symbols"))
     library = store['BINANCE_TEST']
     for symbol in symbols:
         volume = 0.0
@@ -196,10 +216,84 @@ def process_60m_data():
         output_list = [(int(time.time() // 60) * 60000), open, high, low, close, volume]
         if not r.exists(str(int((time.time() // 60) * 60000)) + symbol + "-60m"):
             r.set(str(int((time.time() // 60) * 60000)) + symbol + "-60m", str(json.dumps(output_list)))
-            pd.DataFrame([output_list], columns=['t', 'o', 'h', 'l', 'c', 'v'])
-            library.write(symbol + '-60m', pd)
+            output_list[0] = pd.to_datetime(output_list[0] / 1000, unit='s').tz_localize("UTC")
+            df = pd.DataFrame([output_list], columns=['t', 'o', 'h', 'l', 'c', 'v'])
+            df.set_index('t')
+            library.write(symbol + '-60m', df)
 
     return "60m job executed successfully"
+
+
+@crontab.job(minute="0", hour="0")
+@app.route('/check_data_quality')
+def check_data_quality():
+    library = store['BINANCE_TEST_11']
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    symbols = json.loads(r.get("symbols"))
+    dr = DateRange(datetime.datetime.utcfromtimestamp(time.time()) - datetime.timedelta(hours=1),
+                   datetime.datetime.utcfromtimestamp(time.time()))
+    count = 0
+    with open("report.txt", 'w') as rep:
+        for symbol in symbols:
+            try:
+                rep.write("\n\nFor symbol " + symbol + "\n")
+                df = library.read(symbol + "-1m", date_range=dr).data
+                count = len(df.index)
+                if count < 1440:
+                    rep.write(symbol + "missed 1m data percentage: " + str((1440 - count) * 100 / 1400) + "\n")
+
+                df = library.read(symbol + "-5m", date_range=dr).data
+                count = len(df.index)
+                if count < 288:
+                    rep.write(symbol + "missed 5m data percentage: " + str((288 - count) * 100 / 288) + "\n")
+
+                df = library.read(symbol + "-15m", date_range=dr).data
+                count = len(df.index)
+                if count < 96:
+                    rep.write(symbol + "missed 15m data percentage: " + str((96 - count) * 100 / 96) + "\n")
+
+                df = library.read(symbol + "-30m", date_range=dr).data
+                count = len(df.index)
+                if count < 48:
+                    rep.write(symbol + "missed 30m data percentage: " + str((48 - count) * 100 / 48) + "\n")
+
+                df = library.read(symbol + "-60m", date_range=dr).data
+                count = len(df.index)
+                if count < 24:
+                    rep.write(symbol + "missed 60m data percentage: " + str((24 - count) * 100 / 24) + "\n")
+
+            except Exception as e:
+                count += 1
+    try:
+        print(count)
+
+        sender_address = 'saksham.jain2109@gmail.com'
+        sender_pass = 'Vmc1234$'
+        receiver_address = 'jainsaksham36b@gmail.com'
+        # Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = sender_address
+        message['To'] = receiver_address
+        message['Subject'] = 'Daily report for Fetch data'
+        message.attach(MIMEText("Hello\n please find the daily report", 'plain'))
+        attach_file_name = 'report.txt'
+        attach_file = open(attach_file_name, 'rb')  # Open the file as binary mode
+        payload = MIMEBase('application', 'octate-stream')
+        payload.set_payload((attach_file).read())
+        encoders.encode_base64(payload)  # encode the attachment
+        # add payload header with filename
+        payload.add_header('Content-Decomposition', 'attachment', filename=attach_file_name)
+        message.attach(payload)
+        # Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+        session.starttls()  # enable security
+        session.login(sender_address, sender_pass)  # login with mail_id and password
+        text = message.as_string()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
+    except Exception as e:
+        print(e)
+    return "Job ended"
 
 
 if __name__ == "__main__":
